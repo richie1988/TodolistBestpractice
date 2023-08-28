02fc5a53b5c5053858aa355cde04a260cda012d3
richie1988 <richiessikaonga@gmail.com>
2023-08-28 16:55:58 +0200

    Created the copy of Todolist Best practice

--- src/statusUtils.js
+++ src/statusUtils.js
@@ -1,10 +1,10 @@
-// statusUtils.js
+//
 export default function clearCompletedTasks(tasks) {
-    const updatedTasks = tasks.filter((task) => !task.completed);
-  
-    updatedTasks.forEach((task, index) => {
-      task.index = index + 1;
-    });
-  
-    return updatedTasks;
-  }
+  const updatedTasks = tasks.filter((task) => !task.completed);
+
+  updatedTasks.forEach((task, index) => {
+    task.index = index + 1;
+  });
+
+  return updatedTasks;
+}
