f1dc542430946b26bf0d240db3d5a93322e8e4ae
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-30 23:44:25 +0500

    Add render function

--- src/index.test.js
+++ src/index.test.js
@@ -1,43 +1,53 @@
 // index.test.js
-import { addTask } from './index.js';
+import { addTask, deleteTask } from "./index.js";
 
-describe('To-Do App', () => {
+describe("To-Do App", () => {
   beforeEach(() => {
     // Set up a clean environment before each test
     document.body.innerHTML = '<div id="todo-list"></div>';
     localStorage.clear();
   });
 
-  test('renders tasks correctly', () => {
-
+  test("renders tasks correctly", () => {
     // Write test code to simulate rendering tasks and check the expected behavior
+    const mockTasks = [
+      { description: "Task 1", completed: false },
+      { description: "Task 2", completed: true },
+    ];
+    // Set tasks in localStorage
+    localStorage.setItem("tasks", JSON.stringify(mockTasks));
+    // Call renderTasks to render tasks
+    renderTasks();
+    // Write test assertions to check if tasks are rendered correctly
+    const taskItems = document.querySelectorAll("li");
+    expect(taskItems.length).toBe(mockTasks.length);
   });
 
-  test('adds a task', () => {
+  test("adds a task", () => {
     // Simulate adding a task using the addTask function
-    const newTaskDescription = 'New Task';
+    const newTaskDescription = "New Task";
     addTask(newTaskDescription);
 
     // Check the rendered output in the DOM
-    const taskItems = document.querySelectorAll('li');
+    const taskItems = document.querySelectorAll("li");
     expect(taskItems.length).toBe(1);
 
     const taskItem = taskItems[0];
     const checkbox = taskItem.querySelector('input[type="checkbox"]');
-    const descriptionSpan = taskItem.querySelector('span');
+    const descriptionSpan = taskItem.querySelector("span");
 
     expect(checkbox.checked).toBe(false); // New tasks should be incomplete
     expect(descriptionSpan.textContent).toBe(newTaskDescription);
 
     // Check localStorage to verify the task was saved
-    const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
     expect(savedTasks.length).toBe(1);
     expect(savedTasks[0].description).toBe(newTaskDescription);
     expect(savedTasks[0].completed).toBe(false);
     expect(savedTasks[0].index).toBe(1);
   });
 
-  test('deletes a task', () => {
+  test("deletes a task", () => {
     // Simulate adding tasks using the addTask function
   });
 
