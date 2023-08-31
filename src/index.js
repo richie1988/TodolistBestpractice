37a82fd538845d0571549dc9f557628daf5ed2e3
richie1988 <richiessikaonga@gmail.com>
2023-08-31 14:45:29 +0200

    Fixed the Erros of eslint

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 /* eslint-disable no-use-before-define */
-import "./style.css";
-import clearCompletedTasks from "./statusUtils.js";
+import './style.css';
+import clearCompletedTasks from './statusUtils.js';
 
-const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 export function saveTasks() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 export function deleteTask(indexToDelete) {
-  tasks.splice(indexToDelete, 1);
-  // Update the indices of the remaining tasks
-  for (let i = indexToDelete; i < tasks.length; i += 1) {
-    tasks[i].index = i + 1;
-  }
+  tasks = tasks.filter((task, index) => index !== indexToDelete);
+  tasks = tasks.map((task, index) => {
+    task.index = index + 1;
+    return task;
+  });
 
   saveTasks();
   renderTasks();
 }
 
-function createDeleteButton(index) {
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "X";
-  deleteButton.classList.add("delete-button");
+// export function deleteTask(indexToDelete) {
+//   tasks.splice(indexToDelete, 1);
+//   // Update the indices of the remaining tasks
+//   for (let i = indexToDelete; i < tasks.length; i += 1) {
+//     tasks[i].index = i + 1;
+//   }
 
-  deleteButton.addEventListener("click", () => {
+//   saveTasks();
+//   renderTasks();
+// }
+
+function createDeleteButton(index) {
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.classList.add('delete-button');
+
+  deleteButton.addEventListener('click', () => {
     deleteTask(index);
   });
 
@@ -32,19 +43,19 @@ function createDeleteButton(index) {
 }
 
 export function renderTasks() {
-  const todoList = document.getElementById("todo-list");
+  const todoList = document.getElementById('todo-list');
 
   tasks.sort((a, b) => a.index - b.index);
 
-  todoList.innerHTML = "";
+  todoList.innerHTML = '';
 
   tasks.forEach((task, index) => {
-    const taskItem = document.createElement("li");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
+    const taskItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
     checkbox.checked = task.completed;
 
-    checkbox.addEventListener("change", () => {
+    checkbox.addEventListener('change', () => {
       task.completed = checkbox.checked;
       saveTasks();
       renderTasks();
@@ -52,11 +63,11 @@ export function renderTasks() {
 
     taskItem.appendChild(checkbox);
 
-    const descriptionSpan = document.createElement("span");
+    const descriptionSpan = document.createElement('span');
     descriptionSpan.textContent = task.description;
 
     if (task.completed) {
-      descriptionSpan.classList.add("completed");
+      descriptionSpan.classList.add('completed');
     }
 
     taskItem.appendChild(descriptionSpan);
@@ -85,27 +96,26 @@ export function addTask(description) {
   renderTasks();
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   renderTasks();
 
-  const addButton = document.querySelector(".add-btn");
-  const addInput = document.querySelector(".add-todo");
+  const addButton = document.querySelector('.add-btn');
+  const addInput = document.querySelector('.add-todo');
 
-  addButton.addEventListener("click", () => {
-    if (addInput.value.trim() !== "") {
+  addButton.addEventListener('click', () => {
+    if (addInput.value.trim() !== '') {
       addTask(addInput.value.trim());
-      addInput.value = "";
+      addInput.value = '';
     }
   });
 
-  const clearButton = document.querySelector(".clear-btn");
-  clearButton.addEventListener("click", () => {
-    clearCompletedTasks(tasks);
+  const clearButton = document.querySelector('.clear-btn');
+  clearButton.addEventListener('click', () => {
+    tasks = clearCompletedTasks(tasks);
     renderTasks();
   });
 });
 
-export { tasks };
 export function editTaskDescription(indexToEdit, newDescription) {
   tasks[indexToEdit - 1].description = newDescription;
   saveTasks();
--- src/statusUtils.js
+++ src/statusUtils.js
@@ -1,34 +1,10 @@
-import { renderTasks, saveTasks } from "./index.js";
-//
+// crear function to clear all task completed
 export default function clearCompletedTasks(tasks) {
-  const incompleteTasks = tasks.filter((task) => !task.completed);
-  return incompleteTasks;
-
-  /*const completedTaskIndices = [];
-
-  const updateTasks = tasks.forEach((task, index) => {
-    if (task.completed === true) {
-      completedTaskIndices.push(index);
-    }
-  });
-
-  for (let i = completedTaskIndices.length - 1; i >= 0; i -= 1) {
-    tasks.splice(completedTaskIndices[i], 1);
-  }
-
-  return updateTasks;*/
-
-  /*saveTasks();
-  renderTasks();
-
-  updateIndex();
-  updateLocalStorage();
-  renderTasks();*/
-  /*const updatedTasks = tasks.filter((task) => !task.completed);
+  const updatedTasks = tasks.filter((task) => !task.completed);
 
   updatedTasks.forEach((task, index) => {
     task.index = index + 1;
   });
 
-  return updatedTasks;*/
+  return updatedTasks;
 }
